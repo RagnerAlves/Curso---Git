5d8a7b2757cd09e95c204f1d63146f3823edb010
Ragner <ragnerf@hotmail.com>
2018-01-10 10:28:52 -0200

    Projeto iniciado mostrando msg Ola mundo

---
+++ Projeto_Teste/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_Teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Olá mundo !");
+
+            Console.ReadLine();
+        }
+    }
+}
